2ebadc7ca4553b8616c53be23e3782736019c824
Lázaro <lazaronobrega3@gmail.com>
2022-11-05 20:35:22 -0300

    Adicionando Layout com thymeleaf

--- cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,13 +1,19 @@
 package com.algaworks.cobranca.controller;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.cobranca.model.StatusTitulo;
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.TituloRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/titulos")
@@ -17,8 +23,9 @@ public class TituloController {
 	private TituloRepository tituloRepository;
 
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		return mv;
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
@@ -27,8 +34,17 @@ public class TituloController {
 		tituloRepository.save(titulo);
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem", "Título salvo com sucesso!");
-
 		return mv;
+	}
+	
+	@RequestMapping
+	public String pesquisar() {
+		return "PesquisaTitulo";
+	}
+	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
 	}
 
 }
