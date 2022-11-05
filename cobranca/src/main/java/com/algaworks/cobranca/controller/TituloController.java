da23b11a6fda3872458562a9f56d23fa02c83281
Lázaro <lazaronobrega3@gmail.com>
2022-11-05 16:02:05 -0300

    Salvando títulos no banco H2 de teste, configurando data e valor

--- cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,14 +1,19 @@
 package com.algaworks.cobranca.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.algaworks.cobranca.model.Titulo;
+import com.algaworks.cobranca.repository.TituloRepository;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
+	
+	@Autowired
+	private TituloRepository tituloRepository;
 	
 	@RequestMapping("/novo")
 	public String novo() {
@@ -17,11 +22,8 @@ public class TituloController {
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public String salvar(Titulo titulo) {
-		// Lógica para salvar
-		System.out.println("Descricão do titulo: " + titulo.getCodigo());
-		System.out.println("Descricão do titulo: " + titulo.getDescricao());
 		
-		System.out.println("Descricão do titulo: " + titulo);
+		tituloRepository.save(titulo);
 		
 		return "CadastroTitulo";
 	}
