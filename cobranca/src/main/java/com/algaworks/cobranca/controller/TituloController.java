e3f9d74602cb7ab287bb227181e82819cb4f4a2a
Lázaro <lazaronobrega3@gmail.com>
2022-11-05 15:28:09 -0300

    Enviando dados para o servidor

--- cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -2,12 +2,27 @@ package com.algaworks.cobranca.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.algaworks.cobranca.model.Titulo;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
 	
-	@RequestMapping("/titulo/novo")
+	@RequestMapping("/novo")
 	public String novo() {
+		return "CadastroTitulo";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Titulo titulo) {
+		// Lógica para salvar
+		System.out.println("Descricão do titulo: " + titulo.getCodigo());
+		System.out.println("Descricão do titulo: " + titulo.getDescricao());
+		
+		System.out.println("Descricão do titulo: " + titulo);
+		
 		return "CadastroTitulo";
 	}
 
