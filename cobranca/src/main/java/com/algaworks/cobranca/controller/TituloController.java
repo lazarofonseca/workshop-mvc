985116dd6064b2abe39f87132daa6e2d81ae8585
Lázaro <lazaronobrega3@gmail.com>
2022-12-04 16:24:00 -0300

    Implementando a edição dos titulos

--- cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,43 +1,55 @@
 package com.algaworks.cobranca.controller;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.cobranca.model.StatusTitulo;
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.TituloRepository;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
+
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
 
 	@Autowired
 	private TituloRepository tituloRepository;
 
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(new Titulo());
 		return mv;
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(@Validated Titulo titulo) {
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+
+		if (errors.hasErrors()) {
+			return CADASTRO_VIEW;
+		}
 
 		tituloRepository.save(titulo);
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("mensagem", "Título salvo com sucesso!");
-		return mv;
+
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+		return "redirect:/titulos/novo";
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView pesquisar() {
 		List<Titulo> todosTitulos = tituloRepository.findAll();
@@ -45,9 +57,20 @@ public class TituloController {
 		mv.addObject("titulos", todosTitulos);
 		return mv;
 	}
+
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
 	
+		//Titulo titulo = tituloRepository.getOne(codigoTitulo);
+		
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
+		return mv;
+
+	}
+
 	@ModelAttribute("todosStatusTitulo")
-	public List<StatusTitulo> todosStatusTitulo(){
+	public List<StatusTitulo> todosStatusTitulo() {
 		return Arrays.asList(StatusTitulo.values());
 	}
 
--- cobranca/src/main/java/com/algaworks/cobranca/model/Titulo.java
+++ cobranca/src/main/java/com/algaworks/cobranca/model/Titulo.java
@@ -11,8 +11,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
@@ -23,14 +27,19 @@ public class Titulo {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 
+	@NotEmpty(message = "Descrição é obrigatória.")
+	@Size(max = 60, message = "A descrição não pode conter mais de 60 caracteres")
 	private String descricao;
 
+	@NotNull(message = "Data de vencimento é obrigatório.")
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
 
-	@NotNull
+	@NotNull(message = "Valor é obrigatório.")
 	@NumberFormat(pattern = "#,##0.00")
+	@DecimalMin(value = "0.01", message = "Valor não pode se menor que 0,01")
+	@DecimalMax(value = "9999999.99", message = "Valor não pode se maior que 9.999.999,99")
 	private BigDecimal valor;
 
 	@Enumerated(EnumType.STRING)
